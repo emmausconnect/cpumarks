78e8c5ebf4b8251c04c04b18db0a5487338a0a3e
Paul Ghaleb <pghaleb@emmaus-connect.org>
2025-11-28 09:34:02 +0100

    Bumped version to 1.0.0

--- __version__.py
+++ __version__.py
@@ -1,5 +1,5 @@
 """Version information for cpumarks project"""
-__version__ = "0.1.3"
+__version__ = "1.0.0"
 __authors__ = {
     "Paul": "pghaleb@emmaus-connect.org",
     "Joffrey": "541722+Write@users.noreply.github.com"
